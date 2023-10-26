b09578b18fed3e20d8cadbb1cf4bf749389f9aa4
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2023-10-26 10:03:55 +0200

    add js test

--- person.js
+++ person.js
@@ -28,3 +28,4 @@ class Person {
 }
 
 
+module.exports={Person};
--- personTest.js
+++ personTest.js
@@ -1,1 +1,6 @@
+let {Person} = require ("./person")
+
+let person1 = new Person("Xisca", "Ayala", 1986, 50, 162,  "España", "català", "yoga", "programar", "psicologia");
+
+console.log(person1);
 
